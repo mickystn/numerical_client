e4f6b262c412dea178050ce7cfe6b4c2da1314b7
mickyxoxo <mickystn10@gmail.com>
2022-05-28 17:24:23 +0700

    please3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Chart from './component/chart'
+function App() {
+  return (
+    <div>
+      <Chart />
+    </div>
+      
+    
+  );
+}
+
+export default App;
---
+++ src/component/hello.js
@@ -0,0 +1,8 @@
+//import Chart from '../component/chart'
+
+function hello(){
+    return (
+        <h>1</h>
+    )
+}
+export default hello
